3751c281a8c10ae7625e65eb1e53fd5e851b7668
Takuma Osada <judgment9ttk069@gmail.com>
2019-08-08 15:45:20 +0900

    fix

--- app/src/main/java/jp/co/cyberagent/dojo2019/Presentation/UserShowActivity.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2019/Presentation/UserShowActivity.kt
@@ -3,7 +3,6 @@ package jp.co.cyberagent.dojo2019.Presentation
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -11,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.squareup.picasso.Picasso
 import jp.co.cyberagent.dojo2019.Database.AppDatabase
 import jp.co.cyberagent.dojo2019.Model.User
-import jp.co.cyberagent.dojo2019.Presentation.UserList.UserListActivity
+import jp.co.cyberagent.dojo2019.Presentation.UserIndex.UserIndexActivity
 import jp.co.cyberagent.dojo2019.R
 import kotlin.concurrent.thread
 
@@ -66,19 +65,18 @@ class UserShowActivity : AppCompatActivity() {
     }
 
     private fun createUserFromUri() {
-        Log.d("test", captureURL)
         var captureData = captureURL.split("?")
         var splitData = captureData[1].split("&", "=")
-        user = User.create(splitData[1], splitData[3], splitData[5])
+        user = User.create(splitData[1], splitData[5], splitData[3])
         val userData = user?: return
         thread {
             database?.userDao()?.insert(userData)
         }
-        iamText.text = splitData[1]
-        githubText.text = splitData[3]
-        twitterText.text = splitData[5]
+        iamText.text = user?.iam
+        githubText.text = user?.githubID
+        twitterText.text = user?.twitterID
 
-        val imagepath = "https://github.com/"+ splitData[3] +".png"
+        val imagepath = "https://github.com/"+ splitData[5] +".png"
         Picasso.get()
             .load(imagepath)
             .resize(300, 300)
@@ -87,7 +85,7 @@ class UserShowActivity : AppCompatActivity() {
     }
 
     private fun navigateUserList() {
-        val intent = Intent(this, UserListActivity::class.java)
+        val intent = Intent(this, UserIndexActivity::class.java)
         startActivity(intent)
     }
 
