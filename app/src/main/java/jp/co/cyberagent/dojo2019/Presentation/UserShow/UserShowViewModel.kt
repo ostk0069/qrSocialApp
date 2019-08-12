b938dc57d3ded79e0f9283bf6a18992075a24c91
Takuma Osada <judgment9ttk069@gmail.com>
2019-08-12 14:17:21 +0900

    fix user repository

--- app/src/main/java/jp/co/cyberagent/dojo2019/Presentation/UserShow/UserShowViewModel.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2019/Presentation/UserShow/UserShowViewModel.kt
@@ -3,27 +3,27 @@ package jp.co.cyberagent.dojo2019.Presentation.UserShow
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import jp.co.cyberagent.dojo2019.Database.AppDatabase
 import jp.co.cyberagent.dojo2019.Entity.User
+import jp.co.cyberagent.dojo2019.Repository.UserRepository
 import kotlinx.coroutines.launch
 
 class UserShowViewModel(context: Context) : ViewModel() {
 
-    private val database = AppDatabase.getDatabase(context)
+    private val repository = UserRepository(context)
 
     suspend fun deleteByUid(uid: Int) {
-        database?.userDao()?.deleteByUid(uid)
+        repository.deleteByUid(uid)
     }
 
     suspend fun insertUser(user: User?) {
         val user = user?: return
-        database?.userDao()?.insert(user)
+        repository.insert(user)
     }
 
-    suspend fun deleteUserIfGithubIDAvailable(id: String) {
-        val existUser = database?.userDao()?.findUserByGithubId(id)
+    suspend fun deleteUserIfGithubIDAvailable(githubID: String) {
+        val existUser = repository.findUserBy(githubID)
         if (existUser != null) {
-            database?.userDao()?.deleteByUid(existUser.uid)
+            repository.deleteByUid(existUser.uid)
         }
     }
 
--- app/src/main/java/jp/co/cyberagent/dojo2019/Repository/UserRepository.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2019/Repository/UserRepository.kt
@@ -1,10 +1,26 @@
 package jp.co.cyberagent.dojo2019.Repository
 
+import android.content.Context
 import androidx.lifecycle.LiveData
+import jp.co.cyberagent.dojo2019.Database.AppDatabase
 import jp.co.cyberagent.dojo2019.Database.UserDao
 import jp.co.cyberagent.dojo2019.Entity.User
 
-class UserRepository(private val dao: UserDao) {
+class UserRepository(context: Context) {
+
+    private val database = AppDatabase.getDatabase(context)
+
+    suspend fun insert(user: User) {
+        database.userDao().insert(user)
+    }
+
+    suspend fun deleteByUid(uid: Int) {
+        database.userDao().deleteByUid(uid)
+    }
+
+    suspend fun findUserBy(githubID: String): User? {
+        return database.userDao().findUserByGithubId(githubID)
+    }
 
 //    suspend fun getUsers(): LiveData<List<User>> {
 //        return dao.getLiveUsers()
