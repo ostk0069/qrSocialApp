2c533bc226f0ffd87041488a63017318a983721d
Gumio <quran.gumi3134@gmail.com>
2019-08-13 16:42:22 +0900

    otamesi

--- app/src/main/java/jp/co/cyberagent/dojo2019/Presentation/UserList/UserListViewModel.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2019/Presentation/UserList/UserListViewModel.kt
@@ -1,22 +1,20 @@
 package jp.co.cyberagent.dojo2019.Presentation.UserList
 
 import android.app.Application
-import androidx.lifecycle.*
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import jp.co.cyberagent.dojo2019.Entity.User
 import jp.co.cyberagent.dojo2019.Repository.UserRepository
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
-class UserListViewModel(application: Application): AndroidViewModel(application) {
+class UserListViewModel(application: Application) : AndroidViewModel(application) {
 
     private val repository: UserRepository = UserRepository(application)
 
     fun deleteUser(uid: Int) {
         viewModelScope.launch {
-            withContext(Dispatchers.Main) {
-                repository.deleteByUid(uid)
-            }
+            repository.deleteByUid(uid)
         }
     }
 
